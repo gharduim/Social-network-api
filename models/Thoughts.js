ee8a7c7521e13f39b307a0bf72c8ea78dc1b9b2a
Gustavo <gharduim@gmail.com>
2022-03-02 21:13:50 +1030

    schemas

---
+++ models/Thoughts.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require('mongoose');
+const moment = require('moment')
+const reactions = require('./Reaction')
+
+const thoughtsSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (formatDate) => moment(formatDate).format('dddd, MMMM Do YYYY hh:mm a')
+        },
+        username: {
+            type: String,
+            require: true,
+        },
+        reactions: [reactions]
+    }
+);
+
+thoughtsSchema
+    .virtual('reactionCount')
+    //Counts amount of reactions
+    .get(function () {
+        return this.reactions.length
+    });
+
+const Thoughts = model('thoughts', thoughtsSchema);
+
+module.exports = Thoughts;
---
+++ models/User.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [
+        /^([a-z0-9_\.-]+)@([a-z0-9]+)\.([a-z]{2,6})$/,
+        "Please enter a valid email.",
+      ],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thoughts",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+//virtuals that returns number of friends
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+const User = model('user', userSchema)
+
+module.exports = User;
